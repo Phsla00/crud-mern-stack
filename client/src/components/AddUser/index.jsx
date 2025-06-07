737067bfe8a2f8f2ebebd4dfb8746470d4b7a67d
Paulo Almeida <145488386+Phsla00@users.noreply.github.com>
2025-06-07 12:35:56 -0300

    feat: create form to add user

--- client/src/components/AddUser/index.jsx
+++ client/src/components/AddUser/index.jsx
@@ -1,8 +1,52 @@
 import React from 'react';
+import './index.css';
+import { Link } from 'react-router-dom'
 
 const AddUser = () => {
     return (
-        <div>AddUser</div>
+        <div className="add-user">
+            <Link to="/" type="button" class="btn btn-secondary">
+                <i class="fa-solid fa-arrow-left"></i>
+            </Link>
+            <h3>ADD NEW USER</h3>
+            <form className="add-user_form">
+                <div className="input-group">
+                    <label htmlFor="name">Name:</label>
+                    <input 
+                    type="text"
+                    id="name"
+                    name="name"
+                    autocomplete="off"
+                    placeholder="Enter your name"                    
+                    />
+                </div>
+                <div className="input-group">
+                    <label htmlFor="email">E-mail:</label>
+                    <input 
+                    type="email"
+                    id="email"
+                    name="email"
+                    autocomplete="off"
+                    placeholder="Enter your e-mail"                    
+                    />
+                </div>
+                <div className="input-group">
+                    <label htmlFor="address">Address:</label>
+                    <input 
+                    type="text"
+                    id="address"
+                    name="address"
+                    autocomplete="off"
+                    placeholder="Enter your address"                    
+                    />
+                </div>
+                <div className="input-group">
+                    <button type="button" class="btn btn-primary">
+                        Submit
+                    </button>
+                </div>
+            </form>
+        </div>
     )
 }
 
