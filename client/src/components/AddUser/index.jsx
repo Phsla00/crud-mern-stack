15f3022bf0aca04fddda59d789a0e1ed834a21c5
Paulo Almeida <145488386+Phsla00@users.noreply.github.com>
2025-06-07 17:53:22 -0300

    feat: set up react-hot-toast

--- client/src/components/AddUser/index.jsx
+++ client/src/components/AddUser/index.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import './index.css';
 import { Link, useNavigate } from 'react-router-dom'
 import axios from 'axios';
+import toast from 'react-hot-toast';
 
 const AddUser = () => {
     const users = {
@@ -22,7 +23,7 @@ const AddUser = () => {
         e.preventDefault();
         await axios.post("http://localhost:8000/api/user", user)
         .then((response) => {
-            console.log("User created successfully.");
+            toast.success(response.data.message, {position: "top-right"})
             navigate("/");
         })
         .catch((error) => {
