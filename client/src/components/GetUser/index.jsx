a5832cd2ee1b87a28126e0b5da6d8f03ed94b077
Paulo Almeida <145488386+Phsla00@users.noreply.github.com>
2025-06-18 10:10:06 -0300

    feat: delete user data

--- client/src/components/GetUser/index.jsx
+++ client/src/components/GetUser/index.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import './index.css';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
+import toast from 'react-hot-toast';
 
 const User = () => {
     const [users, setUsers] = useState([]);
@@ -17,42 +18,62 @@ const User = () => {
         fetchData();
     }, []);
     
+    const deleteUser = async (userId) => {
+        await axios.delete(`http://localhost:8000/api/delete/user/${userId}`)
+        .then((response) => {
+            setUsers((prevUser) => prevUser.filter((user) => user._id !== userId));
+            toast.success(response.data.message, {position: "top-right"});
+        })
+        .catch((error) => {
+            console.log(error)
+        })
+    }
+
     return (
         <div className="user-table">
             <Link to="/add" type="button" className="btn btn-primary">
                 <i className="fa-solid fa-user-plus"></i>
             </Link>
-            <table className="table table-bordered">
-                <thead>
-                    <tr>
-                        <th scope="col">S.No.</th>
-                        <th scope="col">Name</th>
-                        <th scope="col">Email</th>
-                        <th scope="col">Address</th>
-                        <th scope="col">Actions</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {users.map((user, index) => {
-                        return (
+
+            {users.length === 0 ? (
+                <div className="no-data">
+                    <h3>No data to display.</h3>
+                    <p>Please, add a new user.</p>
+                </div>
+            ) : (
+                <table className="table table-bordered">
+                    <thead>
                         <tr>
-                            <td>{index+1}</td>
-                            <td>{user.name}</td>
-                            <td>{user.email}</td>
-                            <td>{user.address}</td>
-                            <td className="action-buttons">
-                                <Link to={`/update/` + user._id} type="button" className="btn btn-info">
-                                    <i className="fa-solid fa-pen-to-square"></i>
-                                </Link>
-                                <button type="button" className="btn btn-danger">
-                                    <i className="fa-solid fa-trash"></i>
-                                </button>    
-                            </td>
+                            <th scope="col">S.No.</th>
+                            <th scope="col">Name</th>
+                            <th scope="col">Email</th>
+                            <th scope="col">Address</th>
+                            <th scope="col">Actions</th>
                         </tr>
-                        );
-                    })}
-                </tbody>
-            </table>
+                    </thead>
+                    <tbody>
+                        {users.map((user, index) => {
+                            return (
+                            <tr>
+                                <td>{index+1}</td>
+                                <td>{user.name}</td>
+                                <td>{user.email}</td>
+                                <td>{user.address}</td>
+                                <td className="action-buttons">
+                                    <Link to={`/update/` + user._id} type="button" className="btn btn-info">
+                                        <i className="fa-solid fa-pen-to-square"></i>
+                                    </Link>
+                                    <button onClick={() => deleteUser(user._id)}
+                                    type="button" className="btn btn-danger">
+                                        <i className="fa-solid fa-trash"></i>
+                                    </button>    
+                                </td>
+                            </tr>
+                            );
+                        })}
+                    </tbody>
+                </table>
+            )}
         </div>
     )
 }
